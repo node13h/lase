10ab66791a9e0d6503565f26585bc811e92bcbc5
Sergej Alikov <sergej.alikov@gmail.com>
2019-07-22 22:33:12 +0100

    Correct classifiers
    Licence was incorrect. The correct one is MIT

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Development Status :: 4 - Beta',
+        'OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
 
